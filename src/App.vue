95f5ca262b66a7886c446075a3f46e132be05cc0
Krikinova Olga <shersh.ol@gmail.com>
2022-09-05 17:51:32 +0700

    initial structure

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,59 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <person-list :persons="persons"></person-list>
   </div>
 </template>
 
 <script lang="ts">
+import axios from "axios";
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import { Person } from "./models/person";
+import PersonList from "@/components/PersonList.vue";
+
+const baseURL = "http://localhost:3000/persons";
 
 @Component({
   components: {
-    HelloWorld,
+    PersonList,
   },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  message = "hello";
+  persons: Person[] = [];
+
+  async created() {
+    try {
+      const res = await axios.get(`${baseURL}`);
+      this.persons = res.data;
+    } catch (error) {
+      console.log(`Во время запроза произошла следующая ошибка: ${error}`);
+    }
+  }
+}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+*,
+*::before,
+*::after {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  max-width: 1024px;
+  margin: 0 auto;
+  padding: 15px;
+  font-family: "Arial", sans-serif;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 1.2;
+}
+
+ul {
+  list-style: none;
 }
 </style>
---
+++ src/components/PersonList.vue
@@ -0,0 +1,51 @@
+<template>
+  <ul v-if="persons?.length" class="persons-list">
+    <li
+      v-for="person of persons"
+      v-bind:person="person"
+      :key="person.id"
+      class="person-list__item"
+    >
+      <span>{{ person.firstName }} {{ person.lastName }}</span>
+    </li>
+  </ul>
+  <p v-else class="persons-list persons-list_empty">
+    Список сотрудников пуст. Чтобы добавить, нажмите кнопку ниже
+  </p>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+import { Person } from "@/models/person";
+
+@Component
+export default class PersonList extends Vue {
+  @Prop() readonly persons?: Person[];
+}
+</script>
+
+<style lang="scss">
+.persons-list {
+  margin: 30px 0;
+}
+
+.person-list__item {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0 0 15px;
+  padding: 25px 15px 25px 70px;
+  border-radius: 10px;
+  // background: url('./assets/icons/user-icon.svg') no-repeat 15px center;
+  box-shadow: 0px 0px 4px 0px rgba(34, 60, 80, 0.2);
+}
+
+.persons-list_empty {
+  display: inline-block;
+  vertical-align: baseline;
+  margin: 0 0 15px;
+  padding: 10px;
+  border-radius: 10px;
+  box-shadow: 0px 0px 4px 0px rgba(34, 60, 80, 0.2);
+}
+</style>
